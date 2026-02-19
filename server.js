765616d3afedc9ddb68cfbc94918c499434181d0
oxanashva <oxanashav@gmail.com>
2026-02-19 12:21:32 +0200

    chore: remove unused dev URLs

--- server.js
+++ server.js
@@ -24,12 +24,10 @@ if (process.env.NODE_ENV === 'production') {
 } else {
     const corsOptions = {
         origin: [
-            'http://127.0.0.1:8080',
+            'http://127.0.0.1:3000',
             'http://localhost:3000',
             'http://127.0.0.1:5173',
             'http://localhost:5173',
-            'http://127.0.0.1:5000',
-            'http://localhost:5000',
         ],
         credentials: true
     }
@@ -50,16 +48,7 @@ app.get('/*all', (req, res) => {
     res.sendFile(path.resolve('public/index.html'))
 })
 
-import { logger } from './services/logger.service.js'
-
-// const port = process.env.PORT || 3030
-
-// server.listen(port, () => {
-//     logger.info('Server is running on: ' + `http://localhost:${port}/`)
-// })
-
-// For Docker
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3030;
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on port ${port}`);
 });
