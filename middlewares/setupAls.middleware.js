47f42670870185981a0ed4bbd1cdae099ba5b4d8
oxanashva <oxanashav@gmail.com>
2026-03-11 17:07:23 +0200

    fix: ensure requestId is created in asyncLocalStorage initialization for the logging system

--- middlewares/setupAls.middleware.js
+++ middlewares/setupAls.middleware.js
@@ -1,14 +1,17 @@
 import { authService } from '../api/auth/auth.service.js'
 import { asyncLocalStorage } from '../services/als.service.js'
+import { v4 as uuidv4 } from 'uuid'
 
 export async function setupAsyncLocalStorage(req, res, next) {
 	const storage = {}
 	asyncLocalStorage.run(storage, () => {
+		const alsStore = asyncLocalStorage.getStore()
+		alsStore.requestId = uuidv4()
+
 		if (!req.cookies?.loginToken) return next()
 		const loggedinUser = authService.validateToken(req.cookies.loginToken)
 
 		if (loggedinUser) {
-			const alsStore = asyncLocalStorage.getStore()
 			alsStore.loggedinUser = loggedinUser
 		}
 		next()
